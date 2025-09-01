3969713a3741829453ad9a8df63cc3fc18043faf
jstzaa <fahrizakurniawan51@gmail.com>
2025-09-01 13:42:06 +0700

    update

---
+++ src/latihan5/kel1.java
@@ -0,0 +1,9 @@
+package latihan5;
+
+public class kel1 {
+    public static void main(String[] args) {
+        System.out.println("Kelompok 1");
+        System.out.println("Ajeng Afriyani");
+        System.out.println("Fahriza Kurniawan");
+    }
+}
---
+++ src/latihan5/kelompok1.java
@@ -0,0 +1,9 @@
+package latihan5;
+
+public class kelompok1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 140;
+        System.out.println("Hasil Pnejumlahan bilangan yaitu "+(a*b));
+    }
+}
